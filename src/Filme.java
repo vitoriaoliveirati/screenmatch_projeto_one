93362a6b27d884ebf5541b74d81d407dd3e960c5
Vitória Oliveira <vitoriaoliveir@gmail.com>
2024-04-16 20:27:50 -0300

    Atualizações

--- src/Filme.java
+++ src/Filme.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Filme {
+public class Filme {
+    String nome;
+    int anoDeLancamento;
+    double somaDasAvaliacoes;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+
+    void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+    }
+
+    void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes ++;
+    }
+
+    double pegaMediaDasAvaliacoes(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+
+    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Principal {
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+
+        meuFilme.nome = "A procura da felicidade";
+        meuFilme.anoDeLancamento = 2006;
+        meuFilme.duracaoEmMinutos = 117;
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println(meuFilme.pegaMediaDasAvaliacoes());
+
+    }
 }
